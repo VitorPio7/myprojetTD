2625489fc76d0cd4fd379525b4444d107beee7e5
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-08-26 21:34:10 -0300

    add

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as  banck from './tryCatchs.js'
 import axios from 'axios';
+import pg from 'pg';
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
-const bookId = 1
-const bookPreface = await banck.searchBookbyId(bookId)
+
+const books = new pg.Client({
+    user: 'postgres',
+    password: '10203050',
+    host: 'localhost',
+    port: 5432,
+    database: 'library'
+})
+books.connect()
 
 /*Routes Home*/
 
 app.get('/', async (req, res) => {
-    const bookid = await banck.allBooks()
+    const bookid = (await books.query('SELECT * FROM mybooks')).rows
     res.render("my_books.ejs", {
         bookid: bookid
     })
@@ -21,19 +29,26 @@ app.post('/search', async (req, res) => {
 
     try {
         const bodyReq = req.body.search;
-        const response = await axios.get(`https://openlibrary.org/search.json?q=${bodyReq}&limit=10`);
-        const cover = response.data.docs[2].cover_edition_key
-        const autor = response.data.docs[2].author_alternative_name[11];
-        const date = response.data.docs[2].first_publish_year
-        const pages = response.data.docs[2].first_publish_year
-        const subject = response.data.docs[2].subject_key[Math.round(subject_key.length * Math.random())]
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bodyReq}`);
+        const arrLivros = response.data.items;
+        const random = Math.floor(Math.random() * arrLivros.length);
+        const title = response.data.items[random].volumeInfo.title;
+        const autor = response.data.items[random].volumeInfo.authors[0];
+        const date = response.data.items[random].volumeInfo.publishedDate;
+        const language = response.data.items[random].volumeInfo.language;
+        const pages = response.data.items[random].volumeInfo.pageCount;
+        const description = response.data.items[random].volumeInfo.description;
+        const subject = response.data.items[random].volumeInfo.categories[0];
+        const img = response.data.items[random].volumeInfo.imageLinks.thumbnail
         res.render("preface.ejs", {
-            cover: cover,
             autor: autor,
             date: date,
+            title: title,
+            language: language,
             pages: pages,
+            description: description,
             subject: subject,
-            description:
+            img: img
         })
     }
     catch (err) {
@@ -41,39 +56,38 @@ app.post('/search', async (req, res) => {
     }
 })
 /*Routes my books */
-
-
-/*Routes Preface*/
-app.get('/preface', async (req, res) => {
-    res.render("preface.ejs", {
-        bookPreface: bookPreface
-    })
-})
-app.get('/home', async (req, res) => {
-    res.render("inicial.ejs")
-})
-/*routes My annotations */
-app.get('/MyAnnotations', async (req, res) => {
-    res.render("My_annotations.ejs", {
-        bookPreface: bookPreface
-    })
-})
-app.get('/edit', async (req, res) => {
-    res.render("add_annotations.ejs")
-})
-app.delete('/delete', async (req, res) => {
+app.post('/postBank', async (req, res) => {
+    const title = req.body.title;
+    const autor = req.body.autor;
+    const date = req.body.date;
+    const language = req.body.language;
+    const pages = req.body.pages;
+    const subject = req.body.subject;
+    const description = req.body.description;
+    const img = req.body.img;
+    img.toString()
+    console.log({ title, autor, date, language, pages, subject, description, img })
+    await books.query(
+        'INSERT INTO mybooks (name, autor, language, image, description, type, pages, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+        [title, autor, language, img, description, subject, pages, date]
+    );
+    res.redirect('/')
 
 })
-
-app.get('/add_annotaions', async (req, res) => {
-    res.render("add_annotations.ejs")
-
+app.get('/myAnnotations/:id', async (req, res) => {
+    const idBook = parseInt(req.params.id, 10);
+    if (isNaN(idBook)) {
+        return res.status(400).send('Invalid ID');
+    }
+    console.log(idBook);
+    try {
+        const bookid = (await books.query('SELECT * FROM mybooks WHERE id = $1', [idBook])).rows;
+        res.render('My_annotations.ejs', { bookid: bookid });
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
 })
-app.post('/send', async (req, res) => {
-    const changeText = req.body.inputText;
-    const banckText = await editAnnotationBook(changeText, bookId)
-    res.redirect("/MyAnnotations");
-})
+
 app.listen(3000, () => {
     console.log("porta 3000")
 })
