08816243339469fb9f07f7a5e17269bb6c970d95
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-09-10 14:55:24 -0300

    my first commit

--- index.js
+++ index.js
@@ -3,37 +3,51 @@ import bodyParser from 'body-parser';
 import axios from 'axios';
 import pg from 'pg';
 import session from 'express-session';
-import passport from 'passport'
+import passport from 'passport';
 import { Strategy } from 'passport-local';
+import bcrypt, { hash } from 'bcrypt';
+import env from "dotenv";
 
+env.config();
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const saltRounds = 10;
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 const books = new pg.Client({
-    user: 'postgres',
-    password: '10203050',
-    host: 'localhost',
-    port: 5432,
-    database: 'library'
+    user: process.env.USER_SECRET,
+    password: process.env.USER_PASSWORD,
+    host: process.env.HOST_NAME,
+    port: process.env.PORT_NUMBER,
+    database: process.env.DATABASE_NAME
 })
 books.connect()
 
 /*Routes Home*/
-app.get('/login', (req, res) => {
+app.get('/loginAccount', (req, res) => {
     res.render('loginAndSignUp.ejs')
 })
 
-app.get('/', async (req, res) => {
-    const bookid = (await books.query('SELECT * FROM mybooks')).rows
-    res.render("my_books.ejs", {
-        bookid: bookid
-    })
+app.get('/myBooks', async (req, res) => {
+    try {
+        if (req.isAuthenticated) {
+            const bookid = (await books.query('SELECT * FROM mybooks')).rows
+            res.render("my_books.ejs", {
+                bookid: bookid
+            })
+        } else {
+            res.redirect('/login')
+        }
+    } catch (err) {
+        console.err(err);
+    }
 })
 
 app.post('/search', async (req, res) => {
@@ -68,24 +82,34 @@ app.post('/search', async (req, res) => {
 })
 /*Routes my books */
 app.post('/postBank', async (req, res) => {
-    const title = req.body.title;
-    const autor = req.body.autor;
-    const date = req.body.date;
-    const language = req.body.language;
-    const pages = req.body.pages;
-    const subject = req.body.subject;
-    const description = req.body.description;
-    const img = req.body.img;
-    img.toString()
     try {
-        await books.query(
-            'INSERT INTO mybooks (name, autor, language, image, description, type, pages, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-            [title, autor, language, img, description, subject, pages, date]
-        );
-        res.redirect('/')
+        if (req.isAuthenticated()) {
+            const title = req.body.title;
+            const autor = req.body.autor;
+            const date = req.body.date;
+            const language = req.body.language;
+            const pages = req.body.pages;
+            const subject = req.body.subject;
+            const description = req.body.description;
+            const img = req.body.img;
+            img.toString()
+            try {
+                await books.query(
+                    'INSERT INTO mybooks (name, autor, language, image, description, type, pages, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+                    [title, autor, language, img, description, subject, pages, date]
+                );
+                res.redirect('/')
+            } catch (error) {
+                res.status(404).send(error)
+            }
+        } else {
+            res.redirect("/loginAccount")
+        }
+
     } catch (error) {
-        res.status(404).send(error)
+        console.error(error);
     }
+
 
 })
 app.get('/myAnnotations/:id', async (req, res) => {
@@ -109,7 +133,7 @@ app.get('/remove/:id', async (req, res) => {
     const idBook = parseInt(req.params.id, 10)
     try {
         await books.query('DELETE FROM mybooks WHERE id = $1', [idBook])
-        res.redirect('/')
+        res.redirect('/myBooks')
     } catch (error) {
         res.status(404).send(error)
     }
@@ -133,7 +157,15 @@ app.get('/search-suggestions', async (req, res) => {
     }
 })
 app.get('/home', async (req, res) => {
-    res.redirect("/")
+    try {
+        if (req.isAuthenticated()) {
+            res.redirect("/myBooks")
+        } else {
+            res.redirect("/loginAccount")
+        }
+    } catch (err) {
+        console.err(err);
+    }
 })
 app.post('/edit/:id', async (req, res) => {
     const catchidBody = req.params.id;
@@ -163,6 +195,7 @@ app.post('/delete/:id', async (req, res) => {
 
 })
 app.post('/add-text/:id', async (req, res) => {
+
     const book_id = req.params.id;
     const text_book = req.body.infoText;
 
@@ -174,20 +207,75 @@ app.post('/add-text/:id', async (req, res) => {
     }
 
 });
-app.post('/sendLogin', async (req, res) => {
-    console.log(req.body.email)
-    console.log(req.body.pswd)
+app.post('/login', passport.authenticate("local", {
+    successRedirect: "/myBooks",
+    failureRedirect: "/myBooks"
+}))
+app.post("/signup", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const checkResult = await books.query("SELECT * FROM users WHERE email = $1", [
+            email
+        ]);
+        if (checkResult.rows.length > 0) {
+            req.redirect("/loginAccount")
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.error("Error hashing password:", err);
+                } else {
+                    const result = await books.query("INSERT INTO users (email,password) VALUES ($1, $2) RETURNING *", [email, hash])
+                    const user = result.rows[0];
+                    req.login(user, (err) => {
+                        console.log("sucess");
+                        res.redirect("/myBooks")
+                    })
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
 })
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (!user.verifyPassword(password)) { return done(null, false); }
-            return done(null, user);
-        });
+passport.use("local", new Strategy(
+    async function verify(username, password, cb) {
+        try {
+            const result = await books.query("SELECT * FROM users WHERE email = $1", [
+                username
+            ]);
+            if (result.rows.length > 0) {
+                const user = result.rows[0];
+                const myPlaintextPassword = user.password;
+                bcrypt.compare(password, myPlaintextPassword, function (err, valid) {
+                    if (err) {
+                        console.error("Error comparing passwords:", err);
+                        return cb(err);
+                    } else {
+                        if (valid) {
+                            return cb(null, user);
+                        } else {
+                            return cb(null, false);
+                        }
+                    }
+                });
+            } else {
+                return cb("User not found")
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 ));
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
+
 
 app.listen(3000, () => {
     console.log("porta 3000")
